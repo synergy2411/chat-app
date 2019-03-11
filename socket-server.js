0f7f56b5ddf97dc5255700cae74773b26dad68ab
synergy2411 <synergy2411@outlook.com>
2019-03-11 17:16:33 +0530

    initial commit

---
+++ socket-server.js
@@ -0,0 +1,39 @@
+var express = require("express");
+
+var app = express();
+
+var http = require("http");
+
+var server = http.createServer(app);
+
+var io = require("socket.io")(server);
+
+var messageStack = [];
+
+io.on("connection", client =>{
+    console.log("New client connected...", client.id);
+    client.emit("acknowledge", {data : "Connected"});
+
+    client.on("msgToServer", (chatterName, msg) => {
+        //check if same user
+        messageStack.push(msg);
+        console.log(chatterName + " says : " + msg);
+        client.broadcast.emit("msgToClient", chatterName , msg);
+        client.emit("msgToClient", 'Me', msg);
+    })
+
+    client.on("disconnect", ()=>{
+        console.log("Client disconnected." + client.id);
+        //Save data into DB
+        //messages available in messageStack
+    })
+
+})
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + '/public/socket-client.html');
+})
+
+server.listen(3000, ()=>{
+    console.log("Socket server running on port 3000");
+})
