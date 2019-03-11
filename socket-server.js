addb64949628d36d5a66861c56a1494484d82fb7
synergy2411 <synergy2411@outlook.com>
2019-03-11 17:51:38 +0530

    ready to deploy

--- socket-server.js
+++ socket-server.js
@@ -10,6 +10,8 @@ var io = require("socket.io")(server);
 
 var messageStack = [];
 
+var port = process.env.PORT || 3000;
+
 io.on("connection", client =>{
     console.log("New client connected...", client.id);
     client.emit("acknowledge", {data : "Connected"});
@@ -34,6 +36,6 @@ app.get("/", (req, res)=>{
     res.sendFile(__dirname + '/public/socket-client.html');
 })
 
-server.listen(3000, ()=>{
-    console.log("Socket server running on port 3000");
+server.listen(port, ()=>{
+    console.log("Socket server running on port ", port);
 })
